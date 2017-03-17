e350e38ec55d44eacb4319bdfcf448949aef60fd
yoyo <yuziyao2007@163.com>
2017-03-17 14:31:39 +0800

    add about page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
       post 'downvote'
     end
   end
-
+get'/about/',to:'topics#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
